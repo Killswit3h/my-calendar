3edf49d36b0926a20a525b0bab024303061fe05e
Killswit3h <86539487+Killswit3h@users.noreply.github.com>
2025-09-09 18:09:03 -0400

    refactor employees access and ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import 'leaflet/dist/leaflet.css';
-import Header from "../components/Header";
 
 export const metadata: Metadata = {
   title: "GFC Calendar",
@@ -12,7 +11,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <Header />
         {children}
       </body>
     </html>
